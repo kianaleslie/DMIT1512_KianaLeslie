761afb98bdf02c0f5ca83b80bd37a0f5ea778a7b
kiana <kianaleslie25@gmail.com>
2023-03-22 21:13:42 -0600

    push

--- Lab03_KianaLeslie/Assets/Scripts/ColourChange.cs
+++ Lab03_KianaLeslie/Assets/Scripts/ColourChange.cs
@@ -5,34 +5,30 @@ using UnityEngine;
 public class ColourChange : MonoBehaviour
 {
     //[SerializeField] GameObject ball;
-    [SerializeField] GameObject scoringObject1;
-    [SerializeField] GameObject scoringObject2;
-    [SerializeField] GameObject scoringObject3;
+    [SerializeField] GameObject scoringObject;
+    [SerializeField] public int points;
     public float timer = 0;
     private void Start()
     {
-        timer = 0.5f;
+        timer = 0f;
     }
     void Update()
     {
         if (timer > 0)
         {
-            scoringObject1.GetComponent<Renderer>().material.color = Color.blue;
+            timer -= Time.deltaTime;
+            scoringObject.GetComponent<Renderer>().material.color = Color.magenta;
+        }
+        else
+        {
+            scoringObject.GetComponent<Renderer>().material.color = Color.white;
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ball")
         {
-            scoringObject1.GetComponent<Renderer>().material.color = Color.magenta;
-        }
-        if (collision.gameObject.tag == "Ball")
-        {
-            scoringObject2.GetComponent<Renderer>().material.color = Color.green;
-        }
-        if (collision.gameObject.tag == "Ball")
-        {
-            scoringObject3.GetComponent<Renderer>().material.color = Color.yellow;
+            timer = 0.5F;
         }
     }
 }
