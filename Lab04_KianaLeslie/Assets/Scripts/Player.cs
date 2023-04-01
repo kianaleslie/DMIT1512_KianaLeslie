ca215447fa959de3b308c7e7a39fbc269785f5d2
kiana <kianaleslie25@gmail.com>
2023-03-31 18:31:56 -0600

    updates

--- Lab04_KianaLeslie/Assets/Scripts/Player.cs
+++ Lab04_KianaLeslie/Assets/Scripts/Player.cs
@@ -5,28 +5,55 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] GameObject player;
-    Rigidbody2D rb;
-    private float directionX = 0f;
-    void Start()
+    //[SerializeField] GameObject player;
+    private Rigidbody2D rb;
+    private Animator animate;
+    private SpriteRenderer sprite;
+
+    private float directionX;
+    [SerializeField] private float speed = 7f;
+    [SerializeField] private float jump = 14f;
+    private void Start()
     {
-       rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        animate = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
-    void Update()
+    private void Update()
     {
-        if (Input.GetKey(KeyCode.D) && player.transform.position.x <= 8f)
-        {
-            transform.Translate(new Vector3(7 * Time.deltaTime, 0, 0));
-        }
-        if (Input.GetKey(KeyCode.A) && player.transform.position.x >= -8f)
-        {
-            transform.Translate(new Vector3(-7 * Time.deltaTime, 0, 0));
-        }
+        //if (Input.GetKey(KeyCode.D) && player.transform.position.x <= 8f)
+        //{
+        //    transform.Translate(new Vector3(7 * Time.deltaTime, 0, 0));
+        //}
+        //if (Input.GetKey(KeyCode.A) && player.transform.position.x >= -8f)
+        //{
+        //    transform.Translate(new Vector3(-7 * Time.deltaTime, 0, 0));
+        //}
+
         directionX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(directionX * 7f, rb.velocity.y);
-        if (Input.GetKeyDown(KeyCode.Space))
+        rb.velocity = new Vector2(directionX * speed, rb.velocity.y);
+        if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector3(0, 14, 0);
+            rb.velocity = new Vector2(rb.velocity.x, jump);
+        }
+        UpdateAnimation();
+    }
+    private void UpdateAnimation()
+    {
+        if (directionX > 0f)
+        {
+            animate.SetBool("Running", true);
+            sprite.flipX = false;
+        }
+        else
+            if (directionX < 0f)
+        {
+            animate.SetBool("Running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            animate.SetBool("Running", false);
         }
     }
 }
